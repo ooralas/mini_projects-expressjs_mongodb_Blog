5965554f92b6cde02a0d1197e77f5331aeeef6cc
ooralas <63547913+ooralas@users.noreply.github.com>
2021-12-12 20:18:22 +0100

    update app port

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const blogRoutes = require('./routes/blogRoutes')
 const app = express()
+const Port = process.env.PORT || 3000
 
 //connect to MongoDB
 const dbURI = 'mongodb+srv://salar:456676@trainingcluster.nw0by.mongodb.net/training?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
 .then((result) => {
     console.log("connected to DB")
-    app.listen(3000) // Da wir erstmal mit dem Datenbank verbunden werden und nur dann zu den Requeste listenen, daher packen wir das in dem then-Block, um sicher zu sein, dass unser Anwendung wird nur aktive sein, wenn die Connection zum DB erfolgreich wae 
+    app.listen(Port) // Da wir erstmal mit dem Datenbank verbunden werden und nur dann zu den Requeste listenen, daher packen wir das in dem then-Block, um sicher zu sein, dass unser Anwendung wird nur aktive sein, wenn die Connection zum DB erfolgreich wae 
 })
 .catch((err) => {console.log(err)})
 
